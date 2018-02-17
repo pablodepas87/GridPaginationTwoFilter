eee6b189e5186eb609f8f670e99eebfd3ab57057
pablodepas87 <31258193+pablodepas87@users.noreply.github.com>
2018-02-17 17:30:15 +0100

    Primo Commit
    Primo commit su github

---
+++ filtro.h
@@ -0,0 +1,47 @@
+#ifndef FILTRO_H
+#define FILTRO_H
+#include <QSortFilterProxyModel>
+
+class Filtro : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+public:
+    Filtro(QObject* parent=0);
+     ~Filtro();
+
+    Q_INVOKABLE void setStringaFiltro(QString string);
+    Q_PROPERTY(bool  showOnlyUserRic READ showOnlyUserRic WRITE setshowOnlyUserRic NOTIFY showOnlyUserRicChanged)
+    Q_PROPERTY(QString string READ string WRITE setstring NOTIFY stringChanged)
+
+public slots:
+
+    void setshowOnlyUserRic(bool showOnlyUserRic);
+    bool showOnlyUserRic() const;
+
+    QString string() const;
+    void setstring(QString string);
+
+signals:
+    void showOnlyUserRicChanged();
+    void oggettiFiltChanged();
+
+    void stringChanged();
+
+private:
+
+    bool m_showOnlyUserRic;
+    int m_oggettiFilt;
+
+    QString m_string;
+
+protected:
+    bool filterAcceptsRow(int source_row, const QModelIndex &source_parent) const;
+
+
+    // QSortFilterProxyModel interface
+protected:
+    bool lessThan(const QModelIndex &source_left, const QModelIndex &source_right) const;
+};
+
+#endif // FILTRO_H
